1786e6706a3a9d7230e08878e543c77f2de559fd
Amel Soumia Bouras <amelsoumia245@gmail.com>
2026-04-20 17:47:51 +0100

    Fix sql file typo

--- Scaffolding Files/worldspantry_db.sql
+++ Scaffolding Files/worldspantry_db.sql
@@ -30,7 +30,7 @@ USE worldspantry_db;
 CREATE TABLE `user` (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
     join_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
