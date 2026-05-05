cb18249068d7933f425ae259d3900e026237cc6d
Emmanuel Onyenekwe <leisure-frappe6w@icloud.com>
2026-05-05 13:46:01 +0100

    Update app.js

--- Scaffolding Files/app/app.js
+++ Scaffolding Files/app/app.js
@@ -24,6 +24,10 @@ app.use(session({
     saveUninitialized: false
 }));
 
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
 
 // ==============================
 // ROUTES
